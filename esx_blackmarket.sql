6d0840116839948a232f53f6f6b914a65c6acd24
bynonper00 <54029037+bynonper00@users.noreply.github.com>
2019-08-15 17:25:15 +0300

    item tablosu için id kaldırıldı

--- esx_blackmarket.sql
+++ esx_blackmarket.sql
@@ -22,12 +22,12 @@ CREATE TABLE IF NOT EXISTS `black_muhshipments` (
   `time` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-(3001, 'sarjorpistol', '9mm Sarjor', -1, 0, 1),
-(3002, 'sarjortaramali', 'Taramalı Şarjör', -1, 0, 1),
-(3003, 'sarjorroket', 'Roket', -1, 0, 1),
-(3004, 'sarjorpompali', 'Pompalı Şarjör', -1, 0, 1),
-(3005, 'sarjorkeskin', 'Keskin Nişancı Şarjör', -1, 0, 1),
-(3006, 'sarjoragirtaramali', 'Ağır Taramalı Şarjör', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('sarjorpistol', '9mm Sarjor', -1, 0, 1),
+('sarjortaramali', 'Taramalı Şarjör', -1, 0, 1),
+('sarjorroket', 'Roket', -1, 0, 1),
+('sarjorpompali', 'Pompalı Şarjör', -1, 0, 1),
+('sarjorkeskin', 'Keskin Nişancı Şarjör', -1, 0, 1),
+('sarjoragirtaramali', 'Ağır Taramalı Şarjör', -1, 0, 1);
 
 
